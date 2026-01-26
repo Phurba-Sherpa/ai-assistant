9b24615ac7d3402405685122b9484e056fda18a4
phurba-sherpa <phurba1404@gmail.com>
2026-01-26 17:36:00 +0545

    chore: integrate supabase

--- backend/main.py
+++ backend/main.py
@@ -4,29 +4,46 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_ollama import OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from supabase import Client, create_client
 
 embed_model = "nomic-embed-text"
 file_path = "./caf.pdf"
+
+
+def load_env(key: str) -> str:
+    value = os.getenv(key)
+    if value is None:
+        raise RuntimeError(f"Environment variable {key} not set")
+    return value
+
 
 # Ensure .env content loaded to os env
 load_dotenv(override=True)
 
 # read for os env
-base_url = os.getenv("OLLAMA_BASE_URL")
-
-loader = PyPDFLoader(file_path)
-docs = loader.load()
-
-
-# spit the content
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200, add_start_index=True
-)
-all_splits = text_splitter.split_documents(docs)
+sb_url: str = load_env("SUPABASE_URL")
+sb_key: str = load_env("SUPABASE_KEY")
+base_url = load_env("OLLAMA_BASE_URL")
 
 
-embeddings = OllamaEmbeddings(model=embed_model, base_url=base_url)
-vector_1 = embeddings.embed_query(all_splits[0].page_content)
+supabase: Client = create_client(sb_url, sb_key)
 
-print(f"Generated vectors of length {len(vector_1)}\n")
-print(vector_1[:10])
+resp = supabase.table("faqs").select("*").execute()
+print(resp.data)
+
+# loader = PyPDFLoader(file_path)
+# docs = loader.load()
+#
+#
+# # spit the content
+# text_splitter = RecursiveCharacterTextSplitter(
+#     chunk_size=1000, chunk_overlap=200, add_start_index=True
+# )
+# all_splits = text_splitter.split_documents(docs)
+#
+#
+# embeddings = OllamaEmbeddings(model=embed_model, base_url=base_url)
+# vector_1 = embeddings.embed_query(all_splits[0].page_content)
+#
+# print(f"Generated vectors of length {len(vector_1)}\n")
+# print(vector_1[:10])
